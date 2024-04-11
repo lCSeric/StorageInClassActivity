4e12b43b988cf37609f53f1acbef95b1c4786acf
Eric Lin <tur99299@temple.edu>
2024-04-11 12:19:32 -0400

    saving the comic data in the saveComic function.

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.networkapp
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -14,10 +16,19 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.IOException
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.util.prefs.Preferences
 
 // TODO (1: Fix any bugs)
 // TODO (2: Add function saveComic(...) to save and load comic info automatically when app starts)
 
+private const val AUTO_SAVE_KEY = "auto_save"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var requestQueue: RequestQueue
@@ -27,6 +38,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
 
+    lateinit var preferences: SharedPreferences
+    lateinit var file : File
+    var internalFilename = "my_file"
+    var autoSave = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,6 +53,12 @@ class MainActivity : AppCompatActivity() {
         numberEditText = findViewById<EditText>(R.id.comicNumberEditText)
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
+
+        preferences = getPreferences(MODE_PRIVATE)
+        file = File(filesDir, internalFilename)
+
+        autoSave = preferences.getBoolean(AUTO_SAVE_KEY, false)
+
 
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
@@ -51,13 +72,52 @@ class MainActivity : AppCompatActivity() {
             JsonObjectRequest(url, {showComic(it)}, {
             })
         )
+        if (autoSave){
+            saveComic(comicId)
+        }
     }
 
     private fun showComic (comicObject: JSONObject) {
         titleTextView.text = comicObject.getString("title")
         descriptionTextView.text = comicObject.getString("alt")
+        saveComic(comicObject.getString("title") + "\n" + comicObject )
         Picasso.get().load(comicObject.getString("img")).into(comicImageView)
     }
 
+    private fun saveComic(comicId: String){
+        if (autoSave) {
+            try {
+                val outputStream = FileOutputStream(file)
+                outputStream.write(comicId.toByteArray())
+                outputStream.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
 
-}
+    }
+
+    private fun loadComic(){
+        if (autoSave && file.exists()) {
+            try {
+                val br = BufferedReader(FileReader(file))
+                var line: String?
+                if (br.readLine().also { line = it } != null) {
+                    Log.d("TEST", line.toString())
+                    titleTextView.text = line
+                }
+
+                if (br.readLine().also { line = it } != null) {
+                    Log.d("TEST", line.toString())
+                    descriptionTextView.text = line
+                }
+
+                if (br.readLine().also { line = it } != null) {
+                    Log.d("TEST", line.toString())
+                    Picasso.get().load(line).into(comicImageView)
+                }
+                br.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+    }
